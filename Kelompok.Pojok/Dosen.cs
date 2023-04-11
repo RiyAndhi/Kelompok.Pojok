676fca729be10dc4422588e324e28713e5aafb66
RiyanAndhika <riyanandhi3@gmail.com>
2023-04-12 05:40:51 +0700

    first commit

---
+++ Kelompok.Pojok/Dosen.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kelompok.Pojok
+{
+    class Dosen
+    {
+        private int id;
+        private string nama;
+        private int NIK;
+        private string gender;
+        private string course;
+
+        public Dosen()
+        {
+            id = 1;
+            nama = "Dosen Pengampu";
+            NIK = 12345678;
+            gender = "Male";
+            course = "Pemrograman Berorientasi Objek";
+        }
+
+        public void attDosen(string name, int nik)
+        {
+            nama = name;
+            NIK = nik;
+        }
+
+        public void addCourse(string newCourse)
+        {
+            course += ", " + newCourse;
+        }
+
+        public void display()
+        {
+            Console.WriteLine();
+            Console.WriteLine("ID : " +id);
+            Console.WriteLine("Nama : " + nama);
+            Console.WriteLine("NIK : " + NIK);
+            Console.WriteLine("Gender : " + gender);
+            Console.WriteLine("Course : " + course);
+        } 
+    }
+}
---
+++ Kelompok.Pojok/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kelompok.Pojok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dosen dosen = new Dosen();
+            Console.WriteLine("Default Dosen Pengampu : ");
+            dosen.display();
+
+            Console.WriteLine("\nMasukan Nama dan NIK Dosen Baru : ");
+            dosen.attDosen(Console.ReadLine(), Convert.ToInt32(Console.ReadLine()));
+            dosen.display();
+
+            Console.WriteLine("\nMasukan Mata Kuliah Baru : ");
+            dosen.addCourse(Console.ReadLine());
+            dosen.display();
+
+            Console.ReadLine();
+        }
+    }
+}
